5b2e4bc7adc42fddb07b2cbf15a30837faa48e5d
JSvoldgaard <Jacob.svoldgaard93@gmail.com>
2021-03-23 14:35:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 5555;
+
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Define the schema
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  },
+  type Hurry {
+    herro: String
+  }
+`); 
+
+// Implement resolver functions
+let root = {
+    hello: function () {
+        return 'Hello world!';
+    },
+    herro: function () {
+        return 'Herro world!';
+    }
+}
+
+// Set up an endpoint
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.get('/', function (req, res) {
+    res.send('Express is working!')
+});
+
+app.listen(port, function () {
+    console.log('Listening on port:' + port)
+});
